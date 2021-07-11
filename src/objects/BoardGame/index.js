cf962ff1220413fee508b02487a4016a06e45c0d
FAN-BR <feanetto@yahoo.com.br>
2021-07-11 11:51:27 -0300

    :sparkles: BoardGame object

--- src/objects/BoardGame/index.js
+++ src/objects/BoardGame/index.js
@@ -1,7 +1,12 @@
+import "./style.css"
 import CardGame from "../../components/CardGame";
 function BoardGame(amountCards) {
     const $htmlCardGame = CardGame();
-    const $htmlBoardGame = $htmlCardGame.repeat(amountCards);
-    return $htmlBoardGame;
+    const $htmlContent = $htmlCardGame.repeat(amountCards);
+    return `
+    <section class="board-game">
+        ${$htmlContent}
+    </section>
+    `;
 }
 export default BoardGame;
